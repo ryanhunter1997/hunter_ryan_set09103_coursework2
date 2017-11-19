e646e4756b82629c5918cf1ae12f0fe6b176dcd6
ryanhunter1997 <40206280@live.napier.ac.uk>
2017-11-19 22:14:05 +0000

    concrete and gold album page finished

--- coursework2.py
+++ coursework2.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__,)
 
 @app.route('/')
 def splash():
@@ -12,6 +12,13 @@ def home():
 @app.route('/listen')
 def listen():
     return render_template('listen.html'),200
+
+@app.route('/listen/concreteandgold')
+def candg():
+    return render_template('candg.html'), 200
+@app.route('/listen/sonichighways')
+def sonic():
+    return render_template('sonic.html'), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug = True)
--- static/script.js
+++ static/script.js
@@ -1,3 +1,14 @@
-$('.enter_link').click(function () {
-    $(this).parent('#splashscreen').fadeOut(500);
+$(document).click(function () {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', '../static/concreteandgoldalbum/01. T-Shirt.mp3');
+
+    audioElement.addEventListener('ended', function() {
+        this.play();
+    }, false);
+
+
+    $('#play').click(function() {
+        audioElement.play();
+        $("#status".text("Status: Playing");
+                });
 });
