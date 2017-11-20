5d0c8b17ddc45b86a0d14bc29ff3e88ab7fa6968
ryanhunter1997 <40206280@live.napier.ac.uk>
2017-11-20 19:51:43 +0000

    all albums in listen finshed

--- coursework2.py
+++ coursework2.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-app = Flask(__name__,)
+app = Flask(__name__)
 
 @app.route('/')
 def splash():
@@ -24,6 +24,34 @@ def sonic():
 @app.route('/listen/wastinglight')
 def wasting():
     return render_template('wasting.html'), 200
+
+@app.route('/listen/greatesthits')
+def greatest():
+    return render_template('greatest.html')
+
+@app.route('/listen/espg')
+def echoes():
+    return render_template('echoes.html'), 200
+
+@app.route('/listen/inyourhonor')
+def honor():
+    return render_template('honor.html'), 200
+
+@app.route('/listen/onebyone')
+def one():
+    return render_template('one.html'), 200
+
+@app.route('/listen/tnltl')
+def nothing():
+    return render_template('nothing.html'), 200
+
+@app.route('/listen/tcats')
+def tcats():
+    return render_template('tcats.html'), 200
+
+@app.route('/listen/foofighters')
+def foo():
+    return render_template('foo.html'), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug = True)
